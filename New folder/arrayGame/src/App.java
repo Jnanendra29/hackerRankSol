6299820ca16367eaf646435598b243a654aa6abb
Jnanendra29 <jnanendrasenapati29@gmail.com>
2023-09-20 12:22:05 +0530

    second commit

---
+++ New folder/arrayGame/src/App.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class App {
+
+    public static boolean canWin(int leap, int[] game) {
+        // Return true if you can win the game; otherwise, return false.
+        return canWinFromPosition(0, leap, game);
+    }
+    
+    public static boolean canWinFromPosition(int position, int leap, int[] game) {
+        if(position<0 || game[position] != 0) {
+            return false;
+        }
+        
+        if(position+leap >=game.length || position+1 >= game.length) {
+            return true;
+        }
+        
+        game[position] = -1;
+        
+        return canWinFromPosition(position+leap, leap, game) || 
+            canWinFromPosition(position+1, leap, game) ||
+             canWinFromPosition(position-1, leap, game);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+            
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game)) ? "YES" : "NO" );
+        }
+        scan.close();
+    }
+}
